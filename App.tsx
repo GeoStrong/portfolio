c8ce380105ab9e43154706e7ce2e18aedf56da49
GeoStrong <dzobavageorgij@gmail.com>
2026-01-17 00:11:40 +0400

    Remove theme toggler

--- App.tsx
+++ App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
       <div className="relative">
         <ScrollProgress />
         <QuickNav />
-        <ThemeToggle />
+        {/* <ThemeToggle /> */}
 
         <Suspense
           fallback={
